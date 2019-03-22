78cfc8c1b8b8e401d4948ce83ecce67499d13521
bhupesh68 <sharma.bhupesh@yahoo.com>
2019-03-22 22:51:11 +1100

    comments added to SongDetail

--- generic_project/src/components/songDetail.js
+++ generic_project/src/components/songDetail.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 
-//destructure the props.song property into song.title and song.duration
+{/*1. props = selected song
+//2. destructure the entire props object and only use selectedsong component
+and its its keys -.title and song.duration*/}
 const SongDetail = ({selectedSong}) => {
   if (!selectedSong) {
     return <div><strong>select a song</ strong></div>;
@@ -15,7 +17,11 @@ const SongDetail = ({selectedSong}) => {
   );
 }
 
+{/*in the below {} object, key - "selectedsong" is the prop that the connect
+function will pass into the SongDetail function above as a prop  */}
 const mapStateToProps = state => {
-  return {selectedSong: state.selectedSong};
+  return {selectedSong: state.selectedSong}; //must match with the combine reducer
 }
+{/*for connect, no need for class based component. Functional component is OK.
+  2. no need to pass an action as SongDetail is rendering only when state updates*/}
 export default connect(mapStateToProps)(SongDetail);
--- generic_project/src/components/songsList.js
+++ generic_project/src/components/songsList.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {selectSong} from '../actions';
 
-
 class SongsList extends React.Component {
   renderList(){
     return this.props.songs.map((song) => {
       return (
         <div className="item" key={song.title}><br />
           <div className="clearfix">{song.title}
+            {/*click calling action creator which updates a reducer using dispatch. reducer than
+              updates state inside our redux store. This change in state causes all components
+              hooked up through connect to rerender. Hence, songDetail in App.js rerenders*/}
             <button type="button" className="btn btn-primary float-right"
               onClick={() => this.props.selectSong(song)}>Select
             </button>
@@ -24,7 +26,7 @@ class SongsList extends React.Component {
 
 //map all our data to properties and show inside the SongsList method above
 const mapStateToProps = state => {
-  return {songs:state.songs};
+  return {songs:state.songs}; //must match with the combine reducer
 }
 
 //connect is used to fetch data from the store/provider
